22a6fdabe820943bb76f08faa71d60eb73ccb79d
Aleksey Konovkin <alkon2000@mail.ru>
2016-11-24 17:30:58 +0300

    refactor

--- lua/initd/startup/99-healthcheck.lua
+++ lua/initd/startup/99-healthcheck.lua
@@ -2,50 +2,33 @@ local _M = {
   _VERSION = "1.0.0"
 }
 
-local healthcheck       = require "resty.upstream.dynamic.healthcheck"
-local dynamic_upstream  = require "ngx.dynamic_upstream"
-local stream_upstream   = require "ngx.upstream.stream"
+local http_hc   = require "resty.upstream.dynamic.healthcheck.http"
+local stream_hc = require "resty.upstream.dynamic.healthcheck.stream"
 
 local CONFIG      = ngx.shared.config
 local HEALTHCHECK = "healthcheck"
-
-local function get_upstreams(mod)
-  local ok, upstreams, error = mod.get_upstreams()
-  if not ok then
-    ngx.log(ngx.ERR, "failed to get upstreams: ", err)
-    return nil
-  end
-  return upstreams
-end
 
 local function startup_healthcheck(premature, mod, opts)
   if premature then
     return
   end
 
-  local upstreams = get_upstreams(mod)
-  if not upstreams then
-    return
-  end
+  opts.shm          = HEALTHCHECK;
+  opts.interval     = CONFIG:get("healthcheck.interval");
+  opts.timeout      = CONFIG:get("healthcheck.timeout")
+  opts.fall         = CONFIG:get("healthcheck.fall")
+  opts.rise         = CONFIG:get("healthcheck.rise")
+  opts.concurrency  = 100
+  opts.get_ping_req = opts.get_ping_req
 
-  opts.shm         = HEALTHCHECK;
-  opts.interval    = CONFIG:get("healthcheck.interval");
-  opts.timeout     = CONFIG:get("healthcheck.timeout")
-  opts.fall        = CONFIG:get("healthcheck.fall")
-  opts.rise        = CONFIG:get("healthcheck.rise")
-  opts.concurrency = 100
-
-  for _, service in pairs(upstreams)
-  do
-    ngx.log(ngx.DEBUG, "starting healthchecks for upstream: " .. service)
-
-    opts.upstream = service;
-    opts.http_req = opts.get_ping_req(service);
-
-    local ok, err = healthcheck.spawn_checker(opts)
+  local hc = mod.new(opts)
+  if hc then
+    local ok, err = hc:start()
     if not ok then
-      ngx.log(ngx.ERR, "failed to spawn health checker: ", err)
+      ngx.log(ngx.ERR, "failed to the startup [" .. hc:upstream_type() .. "] healthchecks: ", err)
     end
+  else
+    ngx.log(ngx.ERR, "failed to the create checker")
   end
 end
 
@@ -56,68 +39,20 @@ local function start_job(delay, f, ...)
   end
 end
 
-local _set_peer_down = { http = function(upstream, peer)
-                                  return dynamic_upstream.set_peer_down(upstream, peer.name)
-                                end,
-                         stream = function(upstream, peer)
-                                    local ok, err = stream_upstream.set_peer_down(upstream, peer.is_backup, peer.id, true)
-                                    return ok, nil, err
-                                  end
-                       }
-
-local _get_upstreams = { http = dynamic_upstream.get_upstreams,
-                         stream = function()
-                                    return true, stream_upstream.get_upstreams(), nil
-                                  end
-                       }
-
-local _get_peers     = { http = dynamic_upstream.get_peers,
-                         stream = function(upstream)
-                                    local peers = {}
-                                    local pp = stream_upstream.get_primary_peers(upstream)
-                                    local bp = stream_upstream.get_backup_peers(upstream)
-                                    if pp then
-                                      for _, peer in pairs(pp)
-                                      do
-                                        peer.is_backup = false
-                                        table.insert(peers, peer)
-                                      end
-                                    end
-                                    if bp then
-                                      for _, peer in pairs(bp)
-                                      do
-                                        peer.is_backup = true
-                                        table.insert(peers, peer)
-                                      end
-                                    end
-                                    return true, peers, nil
-                                  end
-                       }
-
-local http_module = { set_peer_down = _set_peer_down.http,
-                      get_upstreams = _get_upstreams.http,
-                      get_peers     = _get_peers.http
-                    }
-
-local stream_module = { set_peer_down = _set_peer_down.stream,
-                        get_upstreams = _get_upstreams.stream,
-                        get_peers     = _get_peers.stream
-                      }
-
-local function set_peers_down(module)
-  local ok, upstreams, error = module.get_upstreams()
+local function set_peers_down(mod)
+  local ok, upstreams, err = mod.get_upstreams()
   if not ok then
     ngx.log(ngx.ERR, "failed to get upstreams: ", err)
     return
   end
 
-  for _, service in pairs(upstreams)
+  for _, u in pairs(upstreams)
   do
-    local ok, servers, err = module.get_peers(service)
+    local ok, peers, err = mod.get_peers(u)
     if ok then
-      for _, server in pairs(servers)
+      for _, peer in pairs(peers)
       do
-        module.set_peer_down(service, server)
+        mod.set_peer_down(u, peer)
       end
     else
       ngx.log(ngx.ERR, "failed to get servers: ", err)
@@ -133,19 +68,19 @@ function _M.startup()
 
   ngx.log(ngx.INFO, "Setup healthcheck job worker #" .. id)
 
-  set_peers_down(http_module)
-  set_peers_down(stream_module)
+  set_peers_down(http_hc)
+  set_peers_down(stream_hc)
 
-  start_job(0, startup_healthcheck, { get_upstreams = http_module.get_upstreams },
-                                    { type = "http",
-                                      get_ping_req = function(service)
-                                        return "GET /" .. service .. "/heartbeat HTTP/1.0\r\n\r\n"
+  start_job(0, startup_healthcheck, http_hc,
+                                    { typ = "http",
+                                      get_ping_req = function(u)
+                                        return "GET /" .. u .. "/heartbeat HTTP/1.0\r\n\r\n"
                                       end,
                                       valid_statuses = { 200, 201, 203, 204 }
                                     })
 
-  start_job(0, startup_healthcheck, { get_upstreams = stream_module.get_upstreams },
-                                    { type = "tcp",
+  start_job(0, startup_healthcheck, stream_hc,
+                                    { typ = "tcp",
                                       get_ping_req = function(service)
                                         return nil
                                       end
