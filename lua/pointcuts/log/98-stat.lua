a29f0d85a060dfe796c1d65f71eedd2856f4b115
Aleksey Konovkin <alkon2000@mail.ru>
2016-11-30 17:44:57 +0300

    request time is nil

--- lua/pointcuts/log/98-stat.lua
+++ lua/pointcuts/log/98-stat.lua
@@ -79,14 +79,22 @@ local function accum_upstream_stat()
   end
 
   local key = u .. "|" .. upstream_status .. "|" .. upstream_addr
-  local start_request_time = ngx.ctx.start_request_time
+  local request_time = ngx.var.request_time
+  local start_request_time
+  
+  if request_time then
+    start_request_time = ngx.now() - request_time
+  else
+    start_request_time = ngx.ctx.start_request_time
+    request_time = ngx.now() - start_request_time
+  end
 
   local ok, err
 
   ok, err    = STAT:safe_add("first_request_time:" .. key, start_request_time, 0) -- first request start time
   ok, err, _ = STAT:set("last_request_time:" .. key, start_request_time, 0)       -- last request start time
   ok, err, _ = STAT:incr("count:" .. key, 1, 0)                                   -- upstream request count by status
-  ok, err, _ = STAT:incr("latency:" .. key, upstream_response_time, 0)            -- upstream total latency by status
+  ok, err, _ = STAT:incr("latency:" .. key, request_time, 0)              -- upstream total latency by status
 
   if not ok then
     error(err)
@@ -108,14 +116,22 @@ local function accum_uri_stat()
   end
 
   local key = "none|" .. (ngx.var.status or 499) .. "|" .. uri
-  local start_request_time = ngx.ctx.start_request_time
+  local request_time = ngx.var.request_time
+  local start_request_time
+  
+  if request_time then
+    start_request_time = ngx.now() - request_time
+  else
+    start_request_time = ngx.ctx.start_request_time
+    request_time = ngx.now() - start_request_time
+  end
 
   local ok, err
 
   ok, err    = STAT:safe_add("first_request_time:" .. key, start_request_time, 0) -- first request start time
   ok, err, _ = STAT:set("last_request_time:" .. key, start_request_time, 0)       -- last request start time
   ok, err, _ = STAT:incr("count:" .. key, 1, 0)                                   -- uri request count by status
-  ok, err, _ = STAT:incr("latency:" .. key, ngx.now() - start_request_time, 0)    -- uri request total latency by status
+  ok, err, _ = STAT:incr("latency:" .. key, request_time, 0)    -- uri request total latency by status
 
   if not ok then
     error(err)
@@ -123,7 +139,7 @@ local function accum_uri_stat()
 
   ngx.log(ngx.DEBUG, "stat pointcut: key=" .. key ..
                      ", start_request_time=" .. start_request_time ..
-                     ", latency=" .. ngx.now() - start_request_time)
+                     ", latency=" .. request_time)
 end
 
 function _M.process()
