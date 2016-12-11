3c7fde985788ae9a556a0c892ccec519bd83c6c0
Aleksey Konovkin <alkon2000@mail.ru>
2016-12-11 04:11:47 +0300

    upstream custom healthcheck

--- conf/conf.d/sysconfig/99-healthcheck.lua
+++ conf/conf.d/sysconfig/99-healthcheck.lua
@@ -5,10 +5,15 @@ local _M = {
 local CONFIG = ngx.shared.config
 
 function _M.config()
-  CONFIG:set("healthcheck.interval", 5000)
-  CONFIG:set("healthcheck.timeout", 5000)
+  CONFIG:set("healthcheck.interval", 10)
+  CONFIG:set("healthcheck.timeout", 1000)
   CONFIG:set("healthcheck.fall", 2)
   CONFIG:set("healthcheck.rise", 2)
+  
+  -- Turn on healthchecks for all peers
+  -- otherwise depends on server healthcheck parameters
+  -- if parameters are missing - no healthchecks
+  CONFIG:set("healthcheck.all", true)
 end
 
 return _M
