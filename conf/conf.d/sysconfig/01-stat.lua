79cc0f01f40d4e2280a98ceacd45aa749117be69
Aleksey Konovkin <Aleksey.Konovkin@billing.ru>
2017-02-06 10:29:35 +0300

    stat config

--- conf/conf.d/sysconfig/01-stat.lua
+++ conf/conf.d/sysconfig/01-stat.lua
@@ -1,21 +1,22 @@
 local _M = {
-  _VERSION = "1.0.0"
+  _VERSION = "1.2.0"
 }
 
 local CONFIG = ngx.shared.config
 
-function _M.preprocess_uri(uri)
-  return uri:gsub("/%d+[^/]+%d+",  "/XXX")
-            :gsub("/%d+",          "/XXX")
-            :gsub("/msisdn:[^/]+", "/msisdn:XXX")
-            :gsub("/msisdn%d+",    "/msisdnXXX")
-            :gsub("C%d+D%d+I%d+",  "XXX")
-            :gsub("/login:[^/]+",  "/login:XXX")
+function _M.preprocess_uri_simple(uri)
+  return uri:gsub("/%d+[^/]+%d+", "/xxx")
+            :gsub("/%d+",         "/xxx")
+end
+
+function _M.preprocess_uri_stub(uri)
+  return uri
 end
 
 function _M.config()
+  CONFIG:set("http.stat.collect_time_min", 1)
   CONFIG:set("http.stat.collect_time_max", 7200)
---CONFIG:set("http.stat.preprocess_uri", "sysconfig.01-stat.preprocess_uri")
+  CONFIG:set("http.stat.preprocess_uri", "sysconfig.01-stat.preprocess_uri_stub")
 end
 
 return _M
