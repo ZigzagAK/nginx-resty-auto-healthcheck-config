f3a1e7418577d20eb4e69c459ab7f7873700d471
Aleksey Konovkin <Aleksey.Konovkin@billing.ru>
2018-04-05 16:14:02 +0300

    optimize job

--- lua/job.lua
+++ lua/job.lua
@@ -29,16 +29,21 @@ end
 
 local main
 
+local function make_key(self, key)
+  return self.key .. "$" .. key
+end
+
 local function set_next_time(self, interval)
-  interval = interval or self.interval
-  JOBS:set(self.key .. ":next", now() + interval)
+  local next_time = now() + (interval or self.interval)
+  JOBS:set(make_key(self, "next"), next_time)
+  return next_time
 end
 
 local function get_next_time(self)
-  local next_time = JOBS:get(self.key .. ":next")
+  local next_time = JOBS:get(make_key(self, "next"))
   if not next_time then
     next_time = now()
-    JOBS:set(self.key .. ":next", next_time)
+    JOBS:set(make_key(self, "next"), next_time)
   end
   return next_time
 end
@@ -60,60 +65,72 @@ local function run_job(self, delay, ...)
   return true
 end
 
+local function wait_others(self)
+  local wait_others = self.wait_others
+  for i=1,#wait_others
+  do
+    if not wait_others[i]:completed(true) then
+      return false
+    end
+  end
+  self.wait_others = {}
+  return true
+end
+
 main = function(premature, self, ...)
   if premature or not self:running() then
     return self:finish(...)
   end
 
-  for _,other in ipairs(self.wait_others)
-  do
-    if not other:completed() then
-      return run_job(self, 0.1, ...)
-    end
+  if not wait_others(self) then
+    return run_job(self, 1, ...)
   end
 
-  local mtx = lock:new("jobs", { timeout = 0.1, exptime = 600 })
-
-  local remains = mtx:lock(self.key .. ":mtx")
+  local remains = self.mutex:lock(make_key(self, "mtx"))
   if not remains then
-    if self:running() then
-      run_job(self, 0.1, ...)
-    end
-    return
+    return run_job(self, 0.2, ...)
   end
 
   if self:suspended() then
-    run_job(self, 0.1, ...)
-    mtx:unlock()
-    return
+    self.mutex:unlock()
+    return run_job(self, 1, ...)
   end
 
   if not self:running() then
-    mtx:unlock()
+    self.mutex:unlock()
     return self:finish(...)
   end
 
   local next_time = get_next_time(self)
 
   if now() >= next_time then
-    local counter = JOBS:incr(self.key .. ":counter", 1, -1)
+    local counter = JOBS:incr(make_key(self, "counter"), 1, -1)
     local ok, err = pcall(self.callback, { counter = counter,
                                            hup = self.pid == nil }, ...)
+
+    ngx_log(DEBUG, "job ", self.key, " counter=", counter)
+
     if not self.pid then
       self.pid = worker_pid()
     end
     if not ok then
-      ngx_log(WARN, self.key, ": ", err)
+      ngx_log(WARN, "job ", self.key, ": ", err)
     end
-    if get_next_time(self) == next_time then
-      -- next time may be changed from outside
-      set_next_time(self)
+
+    local new_next_time = get_next_time(self)
+    if new_next_time == next_time then
+      next_time = set_next_time(self)
+    else
+      -- next time changed from outside
+      next_time = new_next_time
     end
   end
 
-  mtx:unlock()
+  local delay = next_time - now() + random(0, workers - 1) / 10
 
-  run_job(self, get_next_time(self) - now() + random(0, workers - 1) / 10, ...)
+  run_job(self, delay, ...)
+
+  self.mutex:unlock()
 end
 
 --- @type Job
@@ -129,18 +146,21 @@ function _M.new(name, callback, interval, finish)
     interval    = interval,
     key         = name,
     wait_others = {},
-    pid         = nil
+    pid         = nil,
+    cached      = {},
+    mutex       = lock:new("jobs", { timeout = 0.2, exptime = 600, step = 0.2 })
   }
   return setmetatable(j, { __index = job })
 end
 
 --- @param #Job self
 function job:run(...)
-  if not self:completed() then
-    if not self:running() then
+  if not self:completed(true) then
+    if not self:running(true) then
       ngx_log(INFO, "job ", self.key, " start")
-      JOBS:set(self.key .. ":running", 1)
+      JOBS:set(make_key(self, "running"), 1)
     end
+    self:running(true)
     return assert(run_job(self, 0, ...))
   end
   ngx_log(DEBUG, "job ", self.key, " already completed")
@@ -156,7 +176,7 @@ end
 function job:suspend()
   if not self:suspended() then
     ngx_log(INFO, "job ", self.key, " suspended")
-    JOBS:set(self.key .. ":suspended", 1)
+    JOBS:set(make_key(self, "suspended"), 1)
   end
 end
 
@@ -164,30 +184,44 @@ end
 function job:resume()
   if self:suspended() then
     ngx_log(INFO, "job ", self.key, " resumed")
-    JOBS:delete(self.key .. ":suspended")
+    JOBS:delete(make_key(self, "suspended"))
   end
 end
 
 --- @param #Job self
 function job:stop()
-  JOBS:delete(self.key .. ":running")
-  JOBS:set(self.key .. ":completed", 1)
+  JOBS:delete(make_key(self, "running"))
+  JOBS:set(make_key(self, "completed"), 1)
   ngx_log(INFO, "job ", self.key, " stopped")
 end
 
---- @param #Job self
-function job:completed()
-  return JOBS:get(self.key .. ":completed") == 1
+local function check_cached(self, flag, nocache)
+  local sec = now()
+  local c = self.cached[flag]
+  if not c or nocache or sec - c.last > 1 then
+    if not c then
+      c = {}
+      self.cached[flag] = c
+    end
+    c.b = JOBS:get(make_key(self, flag)) == 1
+    c.last = sec
+  end
+  return c.b
 end
 
 --- @param #Job self
-function job:running()
-  return JOBS:get(self.key .. ":running") == 1
+function job:completed(nocache)
+  return check_cached(self, "completed", nocache)
 end
 
 --- @param #Job self
-function job:suspended()
-  return JOBS:get(self.key .. ":suspended") == 1
+function job:running(nocache)
+  return check_cached(self, "running", nocache)
+end
+
+--- @param #Job self
+function job:suspended(nocache)
+  return check_cached(self, "suspended", nocache)
 end
 
 --- @param #Job self
@@ -205,10 +239,10 @@ end
 
 --- @param #Job self
 function job:clean()
-  if not self:running() then
-    JOBS:delete(self.key .. ":running")
-    JOBS:delete(self.key .. ":completed")
-    JOBS:delete(self.key .. ":suspended")
+  if not self:running(true) then
+    JOBS:delete(make_key(self, "running"))
+    JOBS:delete(make_key(self, "completed"))
+    JOBS:delete(make_key(self, "suspended"))
     return true
   end
   return false
